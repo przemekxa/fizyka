d0aa12a3e94ceb5553827b96c9d8aa3f0dde42c3
Przemek Ambroży <przemek19986@gmail.com>
2019-05-01 15:54:47 +0200

    Wyświetlanie wewnątrz klasy Box; powinno działać na Pythonie 3.6

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 import math
 import random
 import pygame
@@ -12,7 +11,7 @@ class Vector:
         self.x = x
         self.y = y
 
-    def __add__(self, other) -> Vector:
+    def __add__(self, other) -> "Vector":
         """ Dodaj dwa wektory do siebie """
         return Vector(self.x + other.x, self.y + other.y)
 
@@ -30,6 +29,7 @@ class Particle:
     """ Pojedyncza cząsteczka o współrzędnych x i y oraz prędkości v
         (w przyszłości można dodać jej promień)
     """
+
     def __init__(self, x: float, y: float, v: Vector):
         self.x = x
         self.y = y
@@ -43,11 +43,11 @@ class Particle:
         self.x += self.v.x
         self.y += self.v.y
 
-    def distance_to(self, to: Particle) -> float:
+    def distance_to(self, to: "Particle") -> float:
         """ Odległość od innej cząsteczki """
         return math.sqrt((self.x - to.x)**2 + (self.y - to.y)**2 )
 
-    def collide_with(self, x: Particle):
+    def collide_with(self, x: "Particle"):
         """ Zderzenie dwóch cząsteczek """
         # TODO
         return
@@ -68,21 +68,24 @@ class Box:
         # Maksymalna wartość składowych prędkości
         self.maxV = max_v
 
-        # Odległośc przy której dochodzi do zderzenia
-        self.collisionDistance = collision_distance
+        # Odległość przy której dochodzi do zderzenia
+        self.collision_distance = collision_distance
+
+        # Okno, w którym cząsteczki będą wyświetlane
+        self.screen = None
 
     def detect_wall_collisions(self):
         """ Wykrywanie i obsługa zderzeń ze ścianami """
         # TODO
         for p in self.particles:
 
-            if p.x <= self.collisionDistance and p.v.x < 0:
+            if p.x <= self.collision_distance and p.v.x < 0:
                 p.v.x *= -1
-            elif p.x >= (self.width - self.collisionDistance) and p.v.x > 0:
+            elif p.x >= (self.width - self.collision_distance) and p.v.x > 0:
                 p.v.x *= -1
-            if p.y <= self.collisionDistance and p.v.y < 0:
+            if p.y <= self.collision_distance and p.v.y < 0:
                 p.v.y *= -1
-            elif p.y >= (self.height - self.collisionDistance) and p.v.y > 0:
+            elif p.y >= (self.height - self.collision_distance) and p.v.y > 0:
                 p.v.y *= -1
 
     def detect_particle_collisions(self):
@@ -90,16 +93,16 @@ class Box:
         for i in range(len(self.particles)):
 
             j = i+1
-            while j<len(self.particles) and self.particles[j].x - self.particles[i].x <= self.collisionDistance:
-                if self.particles[i].distance_to(self.particles[j]) <= self.collisionDistance:
+            while j<len(self.particles) and self.particles[j].x - self.particles[i].x <= self.collision_distance:
+                if self.particles[i].distance_to(self.particles[j]) <= self.collision_distance:
                     self.particles[i].collide_with(self.particles[j])
                 j += 1
 
-    def show_box(self, program_window):
-        """ Pokazywanie pozycji cząsteczek w formie graficznej """
-        program_window.fill((0, 0, 0))
+    def show_box(self):
+        """ Pokazywanie pozycji cząsteczek na ekranie """
+        self.screen.fill((0, 0, 0))
         for particle in self.particles:
-            pygame.draw.circle(program_window, (0, 255, 255), [int(particle.x), int(particle.y)], int(self.collisionDistance))
+            pygame.draw.circle(self.screen, (0, 255, 255), [int(particle.x), int(particle.y)], int(self.collision_distance))
         pygame.display.flip()
 
     def create_particles(self, n: int):
@@ -113,7 +116,7 @@ class Box:
             p = Particle(random.random() * self.startWidth, random.random() * self.height, v)
             self.particles.append(p)
 
-    def simulate(self, program_window=None):
+    def simulate(self):
         """ Symulacja '1 sekundy' ruchu cząsteczek """
 
         # Przesunięcie cząsteczek o wektor
@@ -130,7 +133,7 @@ class Box:
         self.particles.sort(key=lambda particle: particle.x)
 
         # Pokazywanie pozycji cząsteczek
-        self.show_box(program_window)
+        self.show_box()
 
         # Liczenie entropii
         # TODO
@@ -138,21 +141,28 @@ class Box:
         # Pokazywanie wykresu
         # TODO
 
+    def start(self):
+        """ Pętla symulacji """
+
+        # Inicjalizacja PyGame
+        pygame.init()
+
+        # Inicjalizacja okna
+        self.screen = pygame.display.set_mode((int(self.width), int(self.height)))
+
+        end_program = False
+
+        # Pętla programu
+        while not end_program:
+            # Kliknięcie w zamknięcie okna
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    end_program = True
+
+            self.simulate()
+
 
 b = Box(width=800.0, height=600.0, start_width=20, max_v=1.0, collision_distance=2.0)
 b.create_particles(300)
-
-# Start okna
-pygame.init()
-screen = pygame.display.set_mode((int(b.width), int(b.height)))
-end_program = False
-
-# Pętla programu
-while not end_program:
-    # Kliknięcie w zamknięcie okna
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            end_program = True
-
-    b.simulate(screen)
+b.start()
 
